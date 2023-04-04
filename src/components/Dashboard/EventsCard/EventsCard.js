ba6893813977e7d1b89d2eda5eebfd8f0d34796f
iz.aia.h <imukisa024@gmail.com>
2023-04-04 09:28:39 +0300

    can now view which events are on a given day

--- src/components/Dashboard/EventsCard/EventsCard.js
+++ src/components/Dashboard/EventsCard/EventsCard.js
@@ -5,19 +5,29 @@ import { Context } from "../../../contexts/AuthContext";
 export const EventsCard = () => {
   const { events } = useContext(Context);
 
+  const currentDate = new Date();
+  const filteredEvents = events.filter((event) => {
+    const eventDate = new Date(event.start);
+    return (
+      eventDate.getDate() === currentDate.getDate() &&
+      eventDate.getMonth() === currentDate.getMonth() &&
+      eventDate.getFullYear() === currentDate.getFullYear()
+    );
+  });
+
   return (
     <>
       <Card isPressable isHoverable variant="bordered" css={{ mw: "400px" }}>
         <Card.Body>
-          <Text>
-            {events.map((event, key) =>
-              event.start === new Date(Date.now()).toISOString() ? (
-                <span key={key}>{event.title}</span>
-              ) : (
-                <span key={key}>No events for today</span>
-              )
-            )}
-          </Text>
+          {filteredEvents.length > 0 ? (
+            <Text>
+              {filteredEvents.map((event, key) => {
+                return event.title;
+              })}
+            </Text>
+          ) : (
+            <Text>No events scheduled for today</Text>
+          )}
         </Card.Body>
       </Card>
     </>
